b1035fb003974386e1cf7ce7f813eacdc1a48665
Franco Catrin <fcatrin@gmail.com>
2018-06-09 03:20:48 -0400

    Attach processor for output stream

--- src/fcatrin/a8mon/Main.java
+++ src/fcatrin/a8mon/Main.java
@@ -1,6 +1,10 @@
 package fcatrin.a8mon;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 import org.eclipse.swt.widgets.Display;
 
@@ -18,11 +22,41 @@ public class Main {
 		AsyncTask.asyncProcessor = new AsyncProcessor(display);
 		AsyncTask.asyncProcessor.start();
 		
+		Process process = Runtime.getRuntime().exec("atari800");
+		attachProcessors(process);
+		
+		
 		MainWindow mainWindow = new MainWindow(display);
 		mainWindow.open();
 		
 		AsyncTask.asyncProcessor.shutdown();
 		display.dispose();
+	}
+	
+	private static void attachProcessors(final Process process) {
+		Runnable outputProcessor = new Runnable() {
+
+			@Override
+			public void run() {
+				BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+				String line;
+				try {
+					while ((line = in.readLine()) != null) {
+					    System.out.println("XX:" + line);
+					}
+					process.waitFor();
+					System.out.println("ok!");
+	
+					in.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			
+		};
+		
+		Thread t = new Thread(outputProcessor);
+		t.start();
 	}
 	
 }
